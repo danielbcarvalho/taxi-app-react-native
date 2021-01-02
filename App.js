40d10ef0ad385fd0c39a5b85c4dc2759a96793fc
daniel-07 <danielborgesdecarvalho@gmail.com>
2021-01-02 13:03:53 -0300

    add predictions of destinations

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {TextInput, StyleSheet, View, Text} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
-
+import apiKey from './google_api_key';
+import _ from 'lodash';
 import io from 'socket.io-client';
 
 export default class App extends Component {
@@ -12,6 +13,8 @@ export default class App extends Component {
       latitude: 0,
       longitude: 0,
       error: null,
+      destination: '',
+      predictions: [],
     };
   }
 
@@ -29,23 +32,70 @@ export default class App extends Component {
     );
   }
 
+  async onChangeDestination(destination) {
+    //call places API
+    this.setState({destination});
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${destination}&key=${apiKey}&location=${this.state.latitude}, ${this.state.longitude}&radius=2000`;
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions,
+      });
+    } catch (err) {
+      console.log('L', err);
+    }
+  }
+
   render() {
+    const predictions = this.state.predictions.map((prediction) => (
+      <Text style={styles.suggestions} key={prediction.id}>
+        {prediction.description}
+      </Text>
+    ));
+
     return (
-      <MapView
-        style={styles.mapStyle}
-        region={{
-          latitude: this.state.latitude,
-          longitude: this.state.longitude,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}>
-        <Marker coordinate={this.state} />
-      </MapView>
+      <View style={styles.mapStyle}>
+        <MapView
+          style={styles.mapStyle}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}
+        />
+        <TextInput
+          placeholder="Enter destination..."
+          style={styles.destinationInput}
+          value={this.state.destination}
+          onChangeText={(destination) => this.onChangeDestination(destination)}
+        />
+        {predictions}
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  suggestions: {
+    backgroundColor: 'white',
+    fontSize: 14,
+    padding: 5,
+    borderWidth: 0.5,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginRight: 20,
+    marginLeft: 20,
+    padding: 5,
+    backgroundColor: 'white',
+  },
   mapStyle: {
     ...StyleSheet.absoluteFillObject,
   },
---
+++ google_api_key.js
@@ -0,0 +1,3 @@
+const apiKey = 'AIzaSyDMQ7tTJZbny2_nOTgrfcQSa6rPzRsHnEs';
+
+export default apiKey;
