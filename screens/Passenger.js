c220984820bfc3bf9f6dac5a8f63fb24852ee6aa
daniel-07 <danielborgesdecarvalho@gmail.com>
2021-01-02 20:03:31 -0300

    add driver/passenger screen

---
+++ screens/Passenger.js
@@ -0,0 +1,162 @@
+import React, {Component} from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  View,
+  Keyboard,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
+import MapView, {Polyline, Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import apiKey from '../google_api_key';
+import PolyLine from '@mapbox/polyline';
+
+export default class Passenger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      error: null,
+      destination: '',
+      predictions: [],
+      pointCoords: [],
+    };
+  }
+
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+        this.getRouteDirections();
+      },
+      (error) => this.setState({error: error.message}),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 2000},
+    );
+  }
+
+  async getRouteDirections(placeId, destinationName) {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${this.state.latitude},${this.state.longitude}&destination=place_id:${placeId}&key=${apiKey}`,
+      );
+      const json = await response.json();
+      console.log(json);
+      const points = PolyLine.decode(json.routes[0].overview_polyline.points);
+      const pointCoords = points.map((point) => {
+        return {latitude: point[0], longitude: point[1]};
+      });
+      this.setState({
+        pointCoords,
+        predictions: [],
+        destination: destinationName,
+      });
+      Keyboard.dismiss();
+      this.map.fitToCoordinates(pointCoords, {
+        edgePadding: {top: 20, bottom: 20, left: 80, right: 80},
+      });
+    } catch (err) {
+      console.log('Lol', err);
+    }
+  }
+
+  async onChangeDestination(destination) {
+    //call places API
+    this.setState({destination});
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${destination}&key=${apiKey}&location=${this.state.latitude}, ${this.state.longitude}&radius=2000`;
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions,
+      });
+    } catch (err) {
+      console.log('L', err);
+    }
+  }
+
+  render() {
+    let marker = null;
+    if (this.state.pointCoords.length > 1) {
+      marker = (
+        <Marker
+          coordinate={this.state.pointCoords[this.state.pointCoords.length - 1]}
+        />
+      );
+    }
+    const predictions = this.state.predictions.map((prediction) => (
+      <TouchableHighlight
+        onPress={() =>
+          this.getRouteDirections(
+            prediction.place_id,
+            prediction.structured_formatting.main_text,
+          )
+        }
+        key={prediction.place_id}>
+        <View>
+          <Text style={styles.suggestions}>{prediction.description}</Text>
+        </View>
+      </TouchableHighlight>
+    ));
+
+    return (
+      <View style={styles.mapStyle}>
+        <MapView
+          ref={(map) => {
+            this.map = map;
+          }}
+          style={styles.mapStyle}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}>
+          <Polyline
+            coordinates={this.state.pointCoords}
+            strokeWidth={2}
+            strokeColor="red"
+          />
+          {marker}
+        </MapView>
+
+        <TextInput
+          placeholder="Enter destination..."
+          style={styles.destinationInput}
+          value={this.state.destination}
+          onChangeText={(destination) => this.onChangeDestination(destination)}
+        />
+        {predictions}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  suggestions: {
+    backgroundColor: 'white',
+    fontSize: 14,
+    padding: 5,
+    borderWidth: 0.5,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginRight: 20,
+    marginLeft: 20,
+    padding: 5,
+    backgroundColor: 'white',
+  },
+  mapStyle: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
